749acb3b4592d124b5dd144ed7f691d0e7cd882c
makc1235 <makcimmakcim4@gmail.com>
2024-11-28 18:07:46 +0300

    планеты

--- gif6/gif6.js
+++ gif6/gif6.js
@@ -1,7 +1,9 @@
 let xfElement = document.getElementById('hg')
 let ok = document.getElementById('ok')
+let element = document.getElementById("hh")
+
 ok.addEventListener('click', yh )
 
 function yh() {
-
+    element.innerText = xfElement.value
 }
