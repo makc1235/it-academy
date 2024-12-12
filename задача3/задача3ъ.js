3997862eb2651b96fdb82f56eaca53d50a78d7a6
makc1235 <makcimmakcim4@gmail.com>
2024-12-12 17:54:42 +0300

    планеты

---
+++ задача3/задача3ъ.js
@@ -0,0 +1,24 @@
+let xfElement = document.getElementById('d1')
+let span1 = document.getElementById('span1')
+
+function yh() {
+    span1.innerText = xfElement.value
+}
+let jfElement = document.getElementById('d2')
+let span2 = document.getElementById('span2')
+
+function lh() {
+    span2.innerText = jfElement.value
+}
+
+let hfElement = document.getElementById('d3')
+let span3 = document.getElementById('span3')
+
+function hh() {
+    span3.innerText = hfElement.value
+}
+function clearForm() {
+    xfElement.innerText=""
+    jfElement.innerText=""
+    hfElement.innerText=""
+}
